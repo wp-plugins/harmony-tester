69a01029fd301bca2e6e65a32efb5798c9f9c774
neverweep <neverweep@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-01 09:57:46 +0000

    update to 1.30
    git-svn-id: https://plugins.svn.wordpress.org/harmony-tester/tags/1.30@646629 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js.js
@@ -0,0 +1,45 @@
+function ht_check($url) {
+    politic = 'off';
+    porny = 'off';
+    other = 'off';
+    custom = 'off';
+    if (jQuery('#ht_wrap #politic').attr('checked') == 'checked')
+        politic = 'on';
+    if (jQuery('#ht_wrap #porny').attr('checked') == 'checked')
+        porny = 'on';
+    if (jQuery('#ht_wrap #other').attr('checked') == 'checked')
+        other = 'on';
+    if (jQuery('#ht_wrap #custom').attr('checked') == 'checked')
+        custom = 'on';
+    if (jQuery("#wp-content-wrap").hasClass("tmce-active")) {
+        content = tinyMCE.activeEditor.getContent().replace(/\s*/g, '');
+    } else {
+        content = jQuery('#content').val().replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\n/g, '<br/>').replace(/\s*/g, '');
+    }
+    title = jQuery('#title').attr('value');
+    if (title) {
+        content = '<h2 id=ht_test_title>' + jQuery('#title').attr('value').replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\s*/g, '') + '</h2>' + content;
+    }
+    if (content == '')
+        return;
+    jQuery('#ht_wrap #btn').prepend('<img src="' + $url + '/wp-admin/images/wpspin_light.gif" id="ht_loading" />');
+    jQuery('#ht_wrap #ht_check_btn').attr('disabled', 'disabled');
+    jQuery.post($url + '/wp-admin/admin-ajax.php', {
+        action: 'ht_check',
+        content: content,
+        politic: politic,
+        porny: porny,
+        other: other,
+        custom: custom
+    }, function(data) {
+        jQuery('#ht_loading').remove();
+        jQuery('#ht_wrap #ht_check_btn').removeAttr('disabled');
+        if (data) {
+            jQuery('#ht_content').html(data.replace(/style=\\(".*?)\\/g, 'style=$1"').replace(/\\([\'"])/, '$1')).slideDown();
+        } else {
+            jQuery('#ht_content').html('Error 发生错误').slideDown();
+        }
+    }
+    )
+    return;
+}
